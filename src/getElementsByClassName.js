4884dbd71e04b1cfa2dc85c03aa43f3170a829a3
zheshishei <asun0927@gmail.com>
2014-11-25 00:15:01 -0500

    Finished the non-extra credit recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,42 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+  if(this === window) {
+	var html = window.document.children[0];
+	for(var index = 1; index < window.document.children.length; index++) {
+	  if(html.nodeType === 1 && html.nodeName === 'HTML' && typeof html.className !== 'undefined') {
+		break;
+	  }
+	  html = html.nextElementSibling;
+	}
+	
+	html = html.children[0];
+	for(var index = 1; index < html.children.length; index++) {
+	  if(html.nodeType === 1 && html.nodeName === 'BODY' && typeof html.className !== 'undefined') {
+		break;
+	  }
+	  html = html.nextElementSibling;
+	}
+	
+    return getElementsByClassName.call(html, className);
+  }
+  
+  var elements = [];
+  
+  for(var objClass in this.classList) {
+    if(this.nodeName === 'BODY') console.log("body class:" + objClass + "|"  + this.classList[objClass]);
+    if(new RegExp(" " + className + " ").test(this.classList[objClass])) {
+	  console.log("thisPush:" + this.nodeName + "|" + this.classList[objClass]);
+	  elements.push(this);
+	  break;
+	}
+  }
+  
+  for(var child in this.children) {
+	var result = getElementsByClassName.call(this.children[child], className);
+	for(var elem = 0; elem < result.length; elem++) {
+	  elements.push(result[elem]);
+	}
+  }
+  return elements;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,36 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  function stringifyArray(array) {
+    var stringedArr = '[';
+	if(array.length > 0) {
+	  for(var index = 0; index < array.length; index++) {
+	    stringedArr += stringifyJSON(array[index]) + ',';
+	  }
+	  stringedArr = stringedArr.substring(0, stringedArr.length - 1);
+    }
+	stringedArr += ']';
+	return stringedArr;
+  }
+  
+  function stringifyObject(obj) {
+    var stringedObj = '{';
+	for(var item in obj) {
+	  var value = stringifyJSON(obj[item]);
+	  if(value) {
+	     stringedObj += '"' + item + '":' + value + ',';
+	  }
+	}
+	return stringedObj == '{' ? stringedObj + '}' : stringedObj.substring(0, stringedObj.length - 1) + '}';
+  }
+  
+  if(typeof obj === 'function' || typeof obj === 'undefined') {
+   return null;
+  }
+  
+  if(obj == null) {
+    return 'null';
+  }
+  
+  return obj.length !== undefined? (Array.isArray(obj) ? stringifyArray(obj) : '"' + obj.toString() + '"') : typeof obj === 'object' ? stringifyObject(obj) : obj.toString();
 };
